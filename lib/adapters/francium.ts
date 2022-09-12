5cbc5321c742dde2902ae587c2834841e2de50d0
Matt Venables <matt@venabl.es>
2022-09-12 11:05:17 -0400

    Update francium to filter tokens, and greatly increase speed

--- lib/adapters/francium.ts
+++ lib/adapters/francium.ts
@@ -1,7 +1,7 @@
-import { findTokenBySymbol } from "@m2-labs/token-amount"
-import { Connection } from "@solana/web3.js"
+import { findToken, findTokenBySymbol } from "@m2-labs/token-amount"
 import Decimal from "decimal.js"
-import FranciumSDK from "francium-sdk"
+import { lendingPoolList } from "francium-sdk/dist/constants/lend/pools"
+import { getLendingPoolInfo } from "francium-sdk/dist/model"
 import { FetchOptions, ProtocolRates } from "../types"
 import { defaultConnection } from "../utils/connection"
 import { buildAssetRate, buildProtocolRates } from "../utils/rate-fns"
@@ -10,9 +10,17 @@ export const fetch = async ({
   connection = defaultConnection("francium"),
   tokens
 }: FetchOptions = {}): Promise<ProtocolRates> => {
-  const fr = new FranciumSDK({ connection })
+  const desiredTokens = tokens?.length
+    ? tokens.map(findToken).filter(Boolean)
+    : undefined
 
-  const assets = await fr.getLendingPoolInfo()
+  const pools = desiredTokens
+    ? lendingPoolList.filter((p) =>
+        desiredTokens?.find((t) => t?.symbol === p.pool)
+      )
+    : lendingPoolList
+
+  const assets = await getLendingPoolInfo(connection, pools)
 
   const rates = assets.map(({ pool, apy }) => {
     const token = findTokenBySymbol(pool)
