5200f70fed05d1622cfd5e794e7c63aabe986351
Matt Venables <matt@venabl.es>
2022-09-12 12:48:41 -0400

    Update solend to handle filtered tokens

--- lib/adapters/solend-turbo.ts
+++ lib/adapters/solend-turbo.ts
@@ -1,34 +1,35 @@
 import { findTokenByMint } from "@m2-labs/token-amount"
 import { SolendMarket } from "@solendprotocol/solend-sdk"
 import Decimal from "decimal.js"
-import { FetchOptions, ProtocolRates } from "../types"
-import { defaultConnection } from "../utils/connection"
-import { buildAssetRate, buildProtocolRates } from "../utils/rate-fns"
+import { fetchHandler } from "../utils/fetch-fns"
+import { buildAssetRate } from "../utils/rate-fns"
 
-export async function fetch({
-  connection = defaultConnection("solend-turbo"),
-  tokens
-}: FetchOptions = {}): Promise<ProtocolRates> {
-  const market = await SolendMarket.initialize(
-    connection,
-    "production",
-    "7RCz8wb6WXxUhAigok9ttgrVgDFFFbibcirECzWSBauM"
-  )
-  await market.loadReserves()
+export const fetch = fetchHandler(
+  "solend-turbo",
+  async ({ connection, desiredTokens }) => {
+    const market = await SolendMarket.initialize(
+      connection,
+      "production",
+      "7RCz8wb6WXxUhAigok9ttgrVgDFFFbibcirECzWSBauM"
+    )
+    await market.loadReserves()
 
-  const rates = market.reserves.map((reserve) => {
-    const token = findTokenByMint(reserve.config.mintAddress)
+    const rates = market.reserves.map((reserve) => {
+      const token = findTokenByMint(reserve.config.mintAddress)
 
-    if (!token || !reserve.stats) {
-      return
-    }
+      if (!token || !reserve.stats) {
+        return
+      }
 
-    return buildAssetRate({
-      token,
-      deposit: new Decimal(reserve.stats.supplyInterestAPY),
-      borrow: new Decimal(reserve.stats.borrowInterestAPY)
+      if (!desiredTokens || desiredTokens.includes(token)) {
+        return buildAssetRate({
+          token,
+          deposit: new Decimal(reserve.stats.supplyInterestAPY),
+          borrow: new Decimal(reserve.stats.borrowInterestAPY)
+        })
+      }
     })
-  })
 
-  return buildProtocolRates("solend-turbo", rates)
-}
+    return rates
+  }
+)
--- test/adapters/solend.test.ts
+++ test/adapters/solend.test.ts
@@ -12,3 +12,18 @@ test("fetches the solend rates", async () => {
 
   await expectSupported(rates.rates)
 })
+
+test("allows filtering tokens", async () => {
+  const rates = await fetch({ tokens: ["USDC"] })
+
+  expect(rates.protocol).toBe("solend")
+  expect(rates.rates.length).toBeTruthy()
+
+  expect(
+    rates.rates.find(({ token }) => token.symbol === "SOL")
+  ).toBeUndefined()
+
+  expect(rates.rates.find(({ token }) => token.symbol === "USDC")).toBeDefined()
+
+  await expectSupported(rates.rates)
+})
